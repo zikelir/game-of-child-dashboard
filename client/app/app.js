1f889bb92082c30e0c15c7ab7746e0048391dd67
PV Campelo Selano <pv_sf@hotmail.com>
2016-11-27 02:57:37 -0200

    'sassinclusionandangularfix'

--- client/app/app.js
+++ client/app/app.js
@@ -1,15 +1,1 @@
-angular.module('myApp', []);
-
-myApp.controller('mainCtrl', function($scope) {
-  $scope.myvar = 'Hello from HomeController';
-});
-
-
-/*app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-  $routeProvider
-  .when('/', {
-    templateUrl: '/index.html',
-    controller: 'mainCtrl'
-  })
-  $locationProvider.html5Mode(true);
-}]); */
+var myApp = angular.module('myApp', []);
--- client/app/controllers.js
+++ client/app/controllers.js
@@ -1,8 +1,6 @@
-/**
 (function(){
   var myApp = angular.module('myApp', []);
   myApp.controller('mainCtrl', function($scope){
       $scope.myvar = 'helllo';
   });
 })()
-**/
