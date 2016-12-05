c306ff7260150f8987f6630c49dc59a1fee6181e
PV Campelo Selano <pv_sf@hotmail.com>
2016-12-04 23:39:26 -0200

    livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,44 @@
-cdvar gulp = require('gulp');
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
-
-gulp.task('default', function() {
-
-});
+var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  livereload = require('gulp-livereload'),
+  connect = require('gulp-connect');
 
 // watch files for changes and reload
-gulp.task('serve', function() {
-  browserSync({
-    server: {
-      baseDir: 'client'
-    }
-  });
-
-  gulp.watch(['*.html', 'styles/**/*.css', 'app/*.js'], {cwd: 'app'}, reload);
+gulp.task('sass', function() {
+  gulp.src('client/scss/*.scss')
+    .pipe(sass({
+      includePaths: ['client/scss']
+    }))
+    .pipe(gulp.dest('client/css'))
+    .pipe(livereload());
 });
+
+gulp.task('serve', function(done) {
+  var express = require('express');
+  var app = express();
+  app.use(express.static(__dirname + '/client'));
+  app.listen(4000, function () {
+     done();
+  });
+});
+
+gulp.task('connect', function() {
+    connect.server({
+        livereload: true
+    });
+});
+
+gulp.task('html', function() {
+  gulp.src('client/**/*.html')
+    .pipe(livereload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch('client/scss/*.scss', ['sass']);
+  gulp.watch('client/**/*.html', ['html']);
+
+  livereload.listen();
+  console.log('loading');
+});
+
+gulp.task('default', ['watch', 'serve']);
