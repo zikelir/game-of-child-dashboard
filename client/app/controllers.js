09d663fa9a79b7a898725741048eb109dfaf3ce4
Paulo Victor <pselano@br.ibm.com>
2017-04-23 22:31:40 -0300

    fix retraction

--- client/app/controllers.js
+++ client/app/controllers.js
@@ -3,14 +3,15 @@
     myApp.controller('mainCtrl', function($scope) {
 
         $scope.sideMenuClass = "sideMenuHidden";
-        $scope.mainContainerExpanded = "mainContainerExpanded"
+        $scope.mainContainerExpanded = "mainContainerExpanded";
         $scope.changeSideMenuClass = function() {
             if ($scope.sideMenuClass === "sideMenuHidden" && $scope.mainContainerExpanded === "mainContainerExpanded") {
                 $scope.sideMenuClass = "sideMenuShow";
                 $scope.mainContainerExpanded = "mainContainerRetracted";
-            } else
+            } else {
                 $scope.sideMenuClass = "sideMenuHidden";
-            $scope.mainContainerExpanded = "mainContainerExpanded";
+                $scope.mainContainerExpanded = "mainContainerExpanded";
+            }
         }
     });
 })()
