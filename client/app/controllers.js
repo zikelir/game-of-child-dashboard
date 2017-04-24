331ee908a7f40890f6b6876d12da3bc9a88d6c0e
Paulo Victor <pselano@br.ibm.com>
2017-04-23 21:58:03 -0300

    starting menu toggle

--- client/app/controllers.js
+++ client/app/controllers.js
@@ -1,6 +1,14 @@
-(function(){
-  var myApp = angular.module('myApp', []);
-  myApp.controller('mainCtrl', function($scope){
+(function() {
+    var myApp = angular.module('myApp', []);
+    myApp.controller('mainCtrl', function($scope) {
 
-  });
+        $scope.sideMenuClass = "sideMenuHidden";
+        $scope.changeSideMenuClass = function() {
+            if ($scope.sideMenuClass === "sideMenuHidden") {
+                $scope.sideMenuClass = "sideMenuShow";
+            } else
+                $scope.sideMenuClass = "sideMenuHidden";
+        }
+
+    });
 })()
