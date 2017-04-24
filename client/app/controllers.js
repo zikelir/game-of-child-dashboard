39101076a063e41184bf4fb48931e4b4bee64453
Paulo Victor <pselano@br.ibm.com>
2017-04-23 22:07:56 -0300

    finished the transition effect

--- client/app/controllers.js
+++ client/app/controllers.js
@@ -3,12 +3,14 @@
     myApp.controller('mainCtrl', function($scope) {
 
         $scope.sideMenuClass = "sideMenuHidden";
+        $scope.mainContainerExpanded = "mainContainerExpanded"
         $scope.changeSideMenuClass = function() {
-            if ($scope.sideMenuClass === "sideMenuHidden") {
+            if ($scope.sideMenuClass === "sideMenuHidden" && $scope.mainContainerExpanded === "mainContainerExpanded") {
                 $scope.sideMenuClass = "sideMenuShow";
+                $scope.mainContainerExpanded = "mainContainerRetracted";
             } else
                 $scope.sideMenuClass = "sideMenuHidden";
+            $scope.mainContainerExpanded = "mainContainerExpanded";
         }
-
     });
 })()
